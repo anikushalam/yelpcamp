88dbe879d5a52edfd26c0e755b5c4fa772c3f3a0
anikushalam <rjsingh.ak@gmail.com>
2021-05-09 16:35:17 +0530

    after updating try to deploy

--- models/campground.js
+++ models/campground.js
@@ -1,67 +1,69 @@
-const { func } = require('joi');
-const Review = require('./review');
-const mongoose = require('mongoose');
+const Review = require("./review");
+const mongoose = require("mongoose");
 
 //Define only for Schema variable
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    url: String,
-    filename: String
+  url: String,
+  filename: String,
 });
 
-ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
 });
 
-//for include property in mongoose side 
-const opts = { toJSON: { virtuals: true } }
-const CampgroundSchema = new Schema({
+//for include property in mongoose side
+const opts = { toJSON: { virtuals: true } };
+const CampgroundSchema = new Schema(
+  {
     title: String,
     images: [ImageSchema],
     geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User1'
+      type: Schema.Types.ObjectId,
+      ref: "User1",
     },
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        },
-    ]
-}, opts);
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
 
 //for a clustring map so use
 
-CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
-    return `
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `
     <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
     <p>${this.description.substring(0, 20)}...</p > `;
 });
 
 //Deleting work if ew delete show page use findOneAndDelete use only
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    // console.log(doc);
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
-module.exports = mongoose.model('Campground', CampgroundSchema);
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  // console.log(doc);
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+module.exports = mongoose.model("Campground", CampgroundSchema);
